2149668f857bdf8d2920f0b4ed17a3d331854ba4
Yusuke Wada <yusuke@kamawada.com>
2023-09-27 07:42:06 +0900

    add blog link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,17 @@ export const Header = () => {
   return (
     <header>
       <div>
-        <img
-          src='/static/images/profile_childfood_400x400.jpg'
-          alt='profile'
-          width='70'
-          height='70'
-        />
+        <img src="/static/images/profile_childfood_400x400.jpg" alt="profile" width="70" height="70" />
       </div>
       <div>
         <h1>Yusuke Wada</h1>
         <nav>
           <ul>
             <li>
-              <a href='https://twitter.com/yusukebe'>Twitter</a>
-              <a href='https://github.com/yusukebe'>GitHub</a>
-              <a href='https://yusukebe.com'>Blog(ja)</a>
+              <a href="https://twitter.com/yusukebe">Twitter</a>
+              <a href="https://github.com/yusukebe">GitHub</a>
+              <a href="https://blog.yusu.ke">Blog</a>
+              <a href="https://yusukebe.com">Blog(ja)</a>
             </li>
           </ul>
         </nav>
